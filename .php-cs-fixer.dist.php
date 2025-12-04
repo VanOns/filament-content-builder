21164701d53b3f245438c469d1c15680f5198a1a
Maurice Wijnia <maurice@van-ons.nl>
2025-12-04 13:39:08 +0100

    feat: configure cs fixer and phpstan

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,27 @@
+<?php
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'resources',
+        'vendor',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRules([
+        '@PSR12' => true,
+        'array_indentation' => true,
+        'array_syntax' => true,
+        'explicit_indirect_variable' => true,
+        'explicit_string_variable' => true,
+        'method_chaining_indentation' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'phpdoc_line_span' => true,
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder($finder);
