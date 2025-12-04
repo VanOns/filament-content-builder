8dbb825ea2a739989a59deefde5fb2b24bd930d4
Maurice Wijnia <maurice@van-ons.nl>
2025-12-04 13:48:12 +0100

    chore: fix phpstan issues

--- src/Facade/FilamentContentBuilder.php
+++ src/Facade/FilamentContentBuilder.php
@@ -4,6 +4,9 @@ namespace VanOns\FilamentContentBuilder\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @mixin \VanOns\FilamentContentBuilder\FilamentContentBuilder
+ */
 class FilamentContentBuilder extends Facade
 {
     protected static function getFacadeAccessor()
